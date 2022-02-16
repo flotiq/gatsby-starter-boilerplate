fb15aa17002ba530493d8de1e8729142fc0aad29
Kinga <knapik.kinga@gmail.com>
2022-02-16 14:42:46 +0100

    add comments and tips, improve readme, install and use flotiq components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { graphql, useStaticQuery } from 'gatsby';
+import { Button, Header } from 'flotiq-components-react';
 import Logo from '../assets/Logo.svg';
 
+/**
+ * Content of index page
+ */
 const IndexPage = () => {
+    // Extracting data from GraphQL query, the query is on the bottom of this file
     const data = useStaticQuery(query);
     return (
-        <main className="flex flex-col h-screen justify-center items-center">
+        <main className="flex flex-col h-screen justify-center items-center text-light-blue">
+            {/* Content of <head> tag */}
             <Helmet>
                 <html className="bg-dark-blue" lang="en" />
                 <title>{data.allExample.nodes[0].title}</title>
@@ -14,20 +20,38 @@ const IndexPage = () => {
             <div className="m-5">
                 <img src={Logo} alt="Flotiq" width="300px" className="mx-auto" />
             </div>
-            <h1 className="text-center m-24 text-5xl text-light-blue font-bold">
-                {data.allExample.nodes[0].header}
-            </h1>
-            <p className="text-center text-4xl text-light-blue">
-                You can learn more about Flotiq in the
-                {' '}
-                <a href="https://flotiq.com/docs/" target="_blank" rel="noreferrer" className="underline">
-                    documentation
-                </a>
-            </p>
+            {/* Example usage of Header component with text from Flotiq Content Object */}
+            <Header
+                text={data.allExample.nodes[0].header}
+                alignement="center"
+                additionalClasses={['mt-24', 'mb-12']}
+            />
+            {/* Example usage of Header component with static text */}
+            <Header
+                additionalClasses={['mb-5']}
+                level={3}
+                text={(
+                    <>
+                        You can learn more about Flotiq in the
+                        {' '}
+                        <a href="https://flotiq.com/docs/" target="_blank" rel="noreferrer" className="underline">
+                            documentation
+                        </a>
+                    </>
+                )}
+            />
+            <div className="text-center mt-5 pt-5">
+                {/* Example usage of button */}
+                <Button label="Go to Flotiq page" onClick={() => window.open('https://flotiq.com', '_blank').focus()} />
+            </div>
         </main>
     );
 };
 
+/**
+ * GraphQL query getting data for the page
+ * Here it request `header` and `title` properties from `example` content type
+ */
 const query = graphql`
     query IndexQuery {
         allExample {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 module.exports = {
     content: [
+        './node_modules/flotiq-components-react/dist/**/*.{js,jsx,ts,tsx}', // Flotiq Components location
         './src/**/*.{js,jsx,ts,tsx}',
     ],
     theme: {
-        colors: {
-            'light-blue': '#E9F3FF',
-            primary: '#0083FC',
-            'primary-2': '#015BD7',
-            'dark-blue': '#141046',
+        extend: {
+            colors: {
+                'light-blue': '#E9F3FF',
+                primary: '#0083FC',
+                'primary-2': '#015BD7',
+                'dark-blue': '#141046',
+            },
         },
-        extend: {},
     },
-    plugins: [],
+    presets: [
+        require('./node_modules/flotiq-components-react/dist/tailwind.preset'), // Flotiq Component theme presets
+    ],
 };
